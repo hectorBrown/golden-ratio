48c35bf974eda933b6935ba9c2ed624e99f14f04
hectorBrown <45634793+hectorBrown@users.noreply.github.com>
2019-10-24 19:40:50 +0100

    Add files via upload

---
+++ Animated/Animated/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Animated
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.PB_main = new System.Windows.Forms.PictureBox();
+            this.TIM_main = new System.Windows.Forms.Timer(this.components);
+            this.TIM_animate = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.PB_main)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PB_main
+            // 
+            this.PB_main.BackColor = System.Drawing.Color.Black;
+            this.PB_main.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PB_main.Location = new System.Drawing.Point(0, 0);
+            this.PB_main.Name = "PB_main";
+            this.PB_main.Size = new System.Drawing.Size(1404, 812);
+            this.PB_main.TabIndex = 0;
+            this.PB_main.TabStop = false;
+            this.PB_main.Paint += new System.Windows.Forms.PaintEventHandler(this.PB_main_Paint);
+            // 
+            // TIM_main
+            // 
+            this.TIM_main.Enabled = true;
+            this.TIM_main.Interval = 3000;
+            this.TIM_main.Tick += new System.EventHandler(this.TIM_main_Tick);
+            // 
+            // TIM_animate
+            // 
+            this.TIM_animate.Interval = 10;
+            this.TIM_animate.Tick += new System.EventHandler(this.TIM_animate_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1404, 812);
+            this.Controls.Add(this.PB_main);
+            this.Name = "Form1";
+            this.Text = "Golden Ratio";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PB_main)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PB_main;
+        private System.Windows.Forms.Timer TIM_main;
+        private System.Windows.Forms.Timer TIM_animate;
+    }
+}
+
---
+++ Animated/Animated/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Animated
+{
+    public partial class Form1 : Form
+    {
+        Point ratioLine;
+        Point[] positions;
+        int positionIndex = 0;
+        int fibon1, fibon2;
+        Rectangle container;
+        string writeString = "";
+        int n;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Point[] Slide(Point start, Point end, int steps)
+        {
+            Point[] output = new Point[steps];
+            int distX, distY;
+
+            distX = end.X - start.X; distY = end.Y - start.Y;
+            for (int i = 0; i < steps; i++)
+            {
+                int x, y;
+                double t = i / Convert.ToDouble(steps);
+
+                x = Convert.ToInt32(start.X + -Math.Cos(Math.PI * t) * (distX / 2) + (distX / 2));
+                y = Convert.ToInt32(start.Y + -Math.Cos(Math.PI * t) * (distY / 2) + (distY / 2));
+
+                output[i] = new Point(x, y);
+            }
+            return output;
+        }
+
+        private void TIM_main_Tick(object sender, EventArgs e)
+        {
+            GetNextFibon(ref fibon1, ref fibon2);
+            double ratio = Convert.ToDouble(fibon1) / (fibon1 + fibon2);
+            writeString = "n = " + n.ToString() + ", " + (n - 1).ToString() + ": " + fibon1.ToString() + " & " + fibon2.ToString() + ", ratio = " + ratio.ToString()
+                + "\nphi = 0.61803398875";
+            Point next = new Point(Convert.ToInt32(container.Width * ratio + container.Left), container.Top);
+            positions = Slide(ratioLine, next, 100);
+            positionIndex = 0;
+            TIM_animate.Enabled = true;
+            n++;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            fibon1 = 1; fibon2 = 1;
+            container = new Rectangle(100, 100, PB_main.Width - 200, PB_main.Height - 200);
+            ratioLine = new Point(PB_main.Width / 2, container.Top);
+            n = 2;
+            writeString = "n = " + n.ToString() + ", " + (n - 1).ToString() + ": " + fibon1.ToString() + " & " + fibon2.ToString() + ", ratio = 0.5"
+                + "\nphi = 0.61803398875";
+        }
+
+        private void TIM_animate_Tick(object sender, EventArgs e)
+        {
+            ratioLine = positions[positionIndex];
+            positionIndex++;
+            if (positionIndex >= positions.Length)
+            {
+                TIM_animate.Enabled = false;
+            }
+            PB_main.Refresh();
+        }
+
+        private void PB_main_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawString(writeString, SystemFonts.DefaultFont, Brushes.White, 10, 10);
+            e.Graphics.DrawRectangle(Pens.White, container);
+            e.Graphics.DrawLine(Pens.White, ratioLine, new Point(ratioLine.X, container.Bottom));
+        }
+
+        private void GetNextFibon(ref int f1, ref int f2)
+        {
+            int temp = f1;
+            f1 = f1 + f2;
+            f2 = temp;
+        }
+    }
+}
